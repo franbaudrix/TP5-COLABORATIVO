249566dd9b5924e116560e6ff60670cc958391a2
00Karim <166453756+00Karim@users.noreply.github.com>
2024-06-22 18:33:12 -0300

    comentarios calculadora

--- baudrix_velez-P3.sh
+++ baudrix_velez-P3.sh
@@ -1,4 +1,4 @@
-div_cero() {
+div_cero() { #Funcion que evita que se ejecute la division por 0, asi no se detiene el programa, y advierte al usuario del error 
 	echo ""
 	echo "Error! No es posible dividir por 0"
 	echo ""
@@ -14,11 +14,12 @@ div_cero() {
 	done
 }
 
-hacer_cuenta_y_error_handling() {
-	local operacion="$operacion"
-	local titulo="$titulo"
+hacer_cuenta_y_error_handling() { #Funcion que recibe parametros que varian dependiendo de la eleccion del usuario
+#Ejemplo: si es una suma, pasamos el simbolo de sumar, etc.
+	local operacion="$operacion" #Aca recibimos el simbolo correspondiente a la eleccion del usuario
+	local titulo="$titulo" #Y aca recibimos el nombre de la operacion que se eligio para luego mostrarla en los graficos
 	clear
-        graphics "$titulo"
+        graphics "$titulo" 
 	echo ""
         read num1
 	echo ""
@@ -28,20 +29,22 @@ hacer_cuenta_y_error_handling() {
 	echo ""
         read -p "$cad" num2
 	echo ""
-	if [[ $operacion = "/" && $num2 = 0 ]]; then
-		div_cero
-	elif [ "$(echo "$num2" | bc)" != "$num2" ]; then
+	if [[ $operacion = "/" && $num2 = 0 ]]; then #Si la viarable operacion es division y se elige como segundo numero el 0
+	#entonces el usuario esta intentando dividir por 0, por lo que le mostramos un error.	
+  		div_cero
+	elif [ "$(echo "$num2" | bc)" != "$num2" ]; then #Con "bc" hacemos que se interprete el valor como un numero, si el
+ 	#usuario ingreso un dato no numerico, entonces esta accion va a dar error y por consiguiente no va a ser igual a si mismo
                 zenity --error --no-wrap --text="Ingresaste un valor incorrecto,\nsolo podes ingresar numeros!"
-        elif [ "$(echo "$num1" | bc)" != "$num1" ]; then
+        elif [ "$(echo "$num1" | bc)" != "$num1" ]; then #Mismo proceso que el anterior, pero con el otro numero
 		zenity --error --no-wrap --text="Ingresaste un valor incorrecto,\nsolo podes ingresar numeros!"
 	else
 		clear
-        	graphics "$titulo"
+        	graphics "$titulo" #Si el input es correcto, mostramos los graficos y hacemos la cuenta
         	total=$(echo "scale=5; $num1 $operacion $num2" | bc)
 		echo ""
         	echo "$num1 $operacion $num2 = $total"
 		echo ""
-        	while true; do
+        	while true; do #While loop para que el usuario no pueda volver al menu hasta presionar x
                 	echo -n "Ingresa x para volver al menu: "
                 	read -r -n 2 volver
                 	if [[ $volver == $'x' || $volver == $'X' ]]; then
@@ -55,7 +58,7 @@ hacer_cuenta_y_error_handling() {
 
 
 
-graphics() {
+graphics() { #Funcion que muestra los graficos de las distintas opciones de la calculadora 
 	echo "################################"
 	tamanio=${#1}
 	let tamanio=tamanio/2
@@ -69,7 +72,7 @@ graphics() {
 	echo "################################"
 }
 
-while true; do
+while true; do #Graficos para mostrar constantemente el menu
 clear
 graphics "MENU"
 echo "#                              #"
@@ -84,6 +87,7 @@ echo "################################"
 echo -n "Ingresa tu eleccion: "
 read opcion
 
+#Estructura if que deriva al usuario a la funcion para hacer la cuenta con el simbolo y titulo correspondientes a la opcion elegida
 if [ "$opcion" -eq 1 ]; then
 	clear
 	operacion="+"
